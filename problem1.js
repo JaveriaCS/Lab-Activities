af0a73aa96c14e559e96dd30e0b55f0b01537028
Javeria Zulfiqar <100273223+JaveriaCS@users.noreply.github.com>
2022-02-23 02:14:43 -0800

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,25 +1,7 @@
 //1:
-function kthDigitFromLast(n, k) {
-    // If k is less than equal to 0
-    if (k <= 0) {
-        console.log(-1);
-        return;
-    }
-    // Convert integer into string
-    var temp = String(n);
-    // If k is greater than length of the
-    // string temp
-    if (k > temp.length) {
-        console.log(-1);
-    }
-    // Print the k digit from last
-    else {
-        var req = temp.charAt(temp.length - k)
-        // Convert to number again
-        console.log(Number(req));
-    }
-}
-var n = 23617;
-var k = 4;
-// Function call
-kthDigitFromLast(n, k);
+var num = 23617;
+var k =4;
+// Convert integer into string
+var temp=num.toString();
+var kthdigit = temp.charAt(temp.length - k)
+console.log("kth digit of string is : "+" "+kthdigit)
