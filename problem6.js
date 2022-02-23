06268a61b074ce1f9ddf66846ae07c0c1007a78a
Javeria Zulfiqar <100273223+JaveriaCS@users.noreply.github.com>
2022-02-23 01:30:51 -0800

    Add files via upload

---
+++ problem6.js
@@ -0,0 +1,12 @@
+// Activity #Q6:
+
+let value1 = Math.random() * 10;
+//var guess_number = 3;
+var guess_number = 9;
+var inp = 9;
+if (inp == guess_number) { //((guess_number >= 1 || guess_number <= 10)) {
+    console.log('YOU WIN');
+}
+else {
+    console.log('Not Matched');
+}
