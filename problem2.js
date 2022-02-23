74290a6d98b6f8428fe921ab1e8b9a290a7fa524
Javeria Zulfiqar <100273223+JaveriaCS@users.noreply.github.com>
2022-02-23 01:29:39 -0800

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,12 @@
+//Activity #2:
+
+var value = 23617;
+sum = value
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce(function (a, b) {
+        return a + b;
+    }, 0);
+
+console.log(sum);
