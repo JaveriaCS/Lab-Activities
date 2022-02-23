92deb17d92aaad5114ae10dc28f8c689a256a5fe
Javeria Zulfiqar <100273223+JaveriaCS@users.noreply.github.com>
2022-02-23 01:31:37 -0800

    Add files via upload

---
+++ problem8.js
@@ -0,0 +1,6 @@
+// Activity #8:
+function alphabetical_order(str) {
+    return "In alphabetical_order:" + " " + " " + str.split('').sort().join('');
+
+}
+console.log(alphabetical_order("comsats"));
